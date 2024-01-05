909cc32c61075ff67be1eb7ac935fc58483b02ee
Vineetha K <kollamoramv2002@gmail.com>
2024-01-05 11:53:24 +0530

    first commit

---
+++ firstproject/src/App.js
@@ -0,0 +1,113 @@
+import {Component} from 'react'
+
+import {Gallery} from './components/Gallery'
+
+import './App.css'
+
+const imagesList = [
+  {
+    id: 0,
+    imageUrl:
+      'https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-pond-thumbnail-img.png',
+    imageAltText: 'nature mountain with pond',
+    thumbnailAltText: 'nature mountain with pond thumbnail',
+  },
+  {
+    id: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-sunset-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-sunset-thumbnail-img.png',
+    imageAltText: 'nature sunset',
+    thumbnailAltText: 'nature sunset thumbnail',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-thumbnail-img.png',
+    imageAltText: 'nature mountain with ocean',
+    thumbnailAltText: 'nature mountain with ocean thumbnail',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-thumbnail-img.png',
+    imageAltText: 'nature mountain with forest',
+    thumbnailAltText: 'nature mountain with forest thumbnail',
+  },
+  {
+    id: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-leaves-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-leaves-thumbnail-img.png',
+    imageAltText: 'nature leaves',
+    thumbnailAltText: 'nature leaves thumbnail',
+  },
+  {
+    id: 5,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-tree-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-tree-thumbnail-img.png',
+    imageAltText: 'nature tree',
+    thumbnailAltText: 'nature tree thumbnail',
+  },
+  {
+    id: 6,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-thumbnail-img.png',
+    imageAltText: 'nature waterfall',
+    thumbnailAltText: 'nature waterfall thumbnail',
+  },
+  {
+    id: 7,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-river-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-river-thumbnail-img.png',
+    imageAltText: 'nature river',
+    thumbnailAltText: 'nature river thumbnail',
+  },
+]
+
+class App extends Component {
+  state = {
+    activeTabId: imagesList[0].id,
+  }
+
+  clickTabItem = id => {
+    this.setState({activeTabId: id})
+  }
+
+  render() {
+    const {activeTabId} = this.state
+    const {imageUrl, imageAltText} = imagesList[activeTabId]
+
+    return (
+      <div className="card">
+        <div>
+          <img src={imageUrl} alt={imageAltText} className="card1" />
+          <h1>Nature Photography</h1>
+          <p>Nature Photography by Rahul</p>
+          <ul>
+            {imagesList.map(each => (
+              <Gallery
+                key={each.id}
+                details={each}
+                clickTabItem={this.clickTabItem}
+                isActive={activeTabId === each.id}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ firstproject/src/components/ThumbnailItem.js
@@ -0,0 +1,22 @@
+const ThumbnailItem = props => {
+    const {details, clickTabItem, isActive} = props
+    const {id, thumbnailUrl, thumbnailAltText} = details
+    const activeThumb = isActive ? 'card3' : 'card4'
+  
+    const onDelete = () => {
+      clickTabItem(id)
+    }
+  
+    return (
+      <li>
+        <button type="button" onClick={onDelete}>
+          <img
+            src={thumbnailUrl}
+            alt={thumbnailAltText}
+            className={activeThumb}
+          />
+        </button>
+      </li>
+    )
+  }
+  export default ThumbnailItem
